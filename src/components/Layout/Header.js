6f229d3665377cc29ebd4111a5313b00ee7beae3
Sadid <sadid.jones@gmail.com>
2023-04-07 15:12:09 +0600

    006 Adding Individual Meal Items & Displaying Them

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import mealsImages from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+function Header(props) {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton></HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImages} alt="A Table full of Food" />
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,12 @@
+import AvailableMeals from "./AvilableMeals";
+import MealsSummary from "./MealsSummary";
+function Meals() {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+}
+
+export default Meals;
