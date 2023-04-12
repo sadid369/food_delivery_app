7ddac5e70b48ef03f09eae71ce0f64c50e8cdb52
Sadid <sadid.jones@gmail.com>
2023-04-12 20:57:21 +0600

    @12/04/2023

--- src/components/Meals/AvilableMeals.js
+++ src/components/Meals/AvilableMeals.js
@@ -1,4 +1,7 @@
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,11 +30,20 @@ const DUMMY_MEALS = [
 ];
 function AvailableMeals() {
   const mealList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
   return (
     <section className={classes.meals}>
-      <ul>{mealList}</ul>
+      <Card>
+        <ul>{mealList}</ul>
+      </Card>
     </section>
   );
 }
